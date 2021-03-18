f0f00a1f5e311b9af891c802dc6b409c80e92275
Albert <r.albert.huang@gmail.com>
2021-03-18 23:07:28 +0800

    chore: attachments property optional

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,16 @@
 import { Context } from 'probot';
 
 type attachments = {
-    pretext: string,
-    author_name: string,
-    author_icon: string,
-    title: string,
-    title_link: string,
-    text: string,
-    image_url: string,
-    thumb_url: string,
-    footer: string,
-    footer_icon: string
+    pretext?: string
+    author_name?: string
+    author_icon?: string
+    title?: string
+    title_link?: string
+    text?: string
+    image_url?: string
+    thumb_url?: string
+    footer?: string
+    footer_icon?: string
 }
 
 type attachmentsManagement = {
